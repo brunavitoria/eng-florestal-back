9012234b7e044f40bfd56af4cc60c75dbe82a54d
Gabriel Lenz <gabrielxlenz@gmail.com>
2022-07-24 14:58:48 -0300

    amostragens

--- service-julia/server.jl
+++ service-julia/server.jl
@@ -4,6 +4,7 @@ using Genie.Renderer, Genie.Renderer.Html, Genie.Renderer.Json
 include("./controllers/amostragem_simples.jl"); 
 using .AmostragemSimplesController;
 
+<<<<<<< HEAD
 include("./controllers/amostragem_estratificada.jl"); 
 using .AmostragemEstratificadaController;
 
@@ -18,5 +19,8 @@ route("/amostragem-simples", AmostragemSimplesController.process)
 route("/amostragem-estratificada", AmostragemEstratificadaController.process)
 route("/amostragem-independente", AmostragemIndependenteController.process)
 route("/amostragem-sistematica-com-multiplos-inicios-aleatorios", AmostragemMultiplosIniciosAleatoriosController.process)
+=======
+route("/amostragem-simples",  method = POST, AmostragemSimplesController.process)
+>>>>>>> 18c3e61f2315ffe3a9708bdf40a1d20767348ba9
 
 up(8001, async = false)
